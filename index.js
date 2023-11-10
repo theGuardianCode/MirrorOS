20cb1b4ca68320e5c1eca13471da5e6071ae59c0
theGuardianCode <nickrichard878@gmail.com>
2023-11-10 10:12:49 +1000

    Separated styles into file and fixed date time bug

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+const days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
+const months = ["Jan", "Feb", "Mar", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 const updateTime = () => {
 	let element = document.getElementById('time')
 	const date = new Date()
-	const text = date.getHours() % 24 + ":" + date.getMinutes() + "\n" + days[date.getDay() - 1]
+	const text = date.toLocaleTimeString() + "<br>" + days[date.getDay() - 1] + "&nbsp" + date.getDate() + " " + months[date.getMonth() - 1]
 
 	element.innerHTML = text
 }
