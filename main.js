d9507e5577086c22db1fca47e6df266bfdb8b75e
theGuardianCode <nickrichard878@gmail.com>
2023-11-10 17:57:13 +1000

    stuff

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
 	const win = new BrowserWindow({
@@ -6,8 +7,8 @@ const createWindow = () => {
 		height: 600,
 	})
 
-	win.loadFile('index.html')
-	win.removeMenu()
+	win.loadFile('./src/index.html')
+	// win.removeMenu()
 }
 
 app.whenReady().then(() => {
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
+const months = ["Jan", "Feb", "Mar", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+const updateTime = () => {
+	let element = document.getElementById('datetime')
+	const date = new Date()
+	const text = `<div class="time"><h1>${date.toLocaleTimeString()}</h1></div><div class="date"><h3>${days[date.getDay() - 1]}, ${date.getDate()} ${months[date.getMonth() - 1]}</h3></div>`
+
+	element.innerHTML = text
+}
+
+const updateWeather = () => {
+	let element = document.getElementById('weather')
+
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=Cairns&appid=4bbd8c76efb20600772acc0c381b9dc8&units=metric`
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			const type = data["weather"][0]["main"]
+			const temp = data["main"]["temp"]
+				
+			const text = `<h1>${temp}&#176C</h1>`
+			element.innerHTML = text
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
+
+setTimeout(updateWeather, 1000)
+
+setInterval(updateTime, 1000)
+setInterval(updateWeather, 30 * 60 * 1000)
